cb4511e96c31453db46400e5f3ca64ca3a5af665
Thomas Ballinger <thomasballinger@gmail.com>
2015-02-27 19:35:11 -0500

    we wrote react

--- source.js
+++ source.js
@@ -2,3 +2,21 @@ console.log('asdf');
 
 
 
+function render(list_of_stuff){
+  var view = document.getElementById('view');
+  view.innerHTML = '';
+  for (var i = 0; i < list_of_stuff.length; i++){
+    view.appendChild(item(list_of_stuff[i].name));
+  }
+}
+  
+function item(name){
+  div = document.createElement('div');
+  div.className = 'item';
+  return div
+}
+
+
+stuff = [{'name': 'Tom'}, {'name': 'Lyn'}]
+
+setInterval(function(){render(stuff)}, 1000);
