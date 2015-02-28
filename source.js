f7d76f30bf8b9729a236564ea80e268e47ecac7a
Lyn Nagara <lyn.nagara@gmail.com>
2015-02-27 19:57:05 -0500

    renders components

--- source.js
+++ source.js
@@ -1,22 +1,45 @@
-console.log('asdf');
+// Store
+var store = {
+	todoItems: [{'name': 'Tom'}, {'name': 'Lyn'}]
 
-
-
-function render(list_of_stuff){
-  var view = document.getElementById('view');
-  view.innerHTML = '';
-  for (var i = 0; i < list_of_stuff.length; i++){
-    view.appendChild(item(list_of_stuff[i].name));
-  }
 }
+
+
+
+var component1 = {
+	state: {
+		name: 'Tom'
+	},
+	template: '<div>{{state.name}}</div>'
+}
+
+
+function renderComponent(component) {
+	var view = document.getElementById('view');
+	view.innerHTML = '';
+	var element = document.createElement('div');
+	element.innerHTML = component.template;
+	view.appendChild(element);
+}
+
+renderComponent(component1);
+
+
+// function render(list_of_stuff){
+//   var view = document.getElementById('view');
+//   view.innerHTML = '';
+//   for (var i = 0; i < list_of_stuff.length; i++){
+//     view.appendChild(item(list_of_stuff[i].name));
+//   }
+// }
   
-function item(name){
-  div = document.createElement('div');
-  div.className = 'item';
-  return div
-}
+// function item(name){
+//   div = document.createElement('div');
+//   div.className = 'item';
+//   return div
+// }
 
 
-stuff = [{'name': 'Tom'}, {'name': 'Lyn'}]
+// stuff = [{'name': 'Tom'}, {'name': 'Lyn'}]
 
-setInterval(function(){render(stuff)}, 1000);
+// setInterval(function(){render(stuff)}, 1000);
